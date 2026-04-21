5e8ebf1fd93f4fa1159acaa3cdd228e692968694
Yogendra Pratap Singh <yogendrapratapsingh70@gmail.com>
2026-04-21 18:11:35 +0530

    terratest cases added for vpc and subnets

---
+++ tests/vpc_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVPCModule(t *testing.T) {
+
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../", // root where your main.tf exists
+
+		VarFiles: []string{
+			"environment/dev-nagarro-promotion/terraform.tfvars",
+		},
+	}
+
+	// Cleanup after test
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Init + Apply
+	terraform.InitAndApply(t, terraformOptions)
+
+	// ---- VALIDATIONS ----
+
+	// 1. VPC ID should exist
+	vpcID := terraform.Output(t, terraformOptions, "vpc_id")
+	assert.NotEmpty(t, vpcID)
+
+	// 2. CIDR validation
+	vpcCIDR := terraform.Output(t, terraformOptions, "vpc_cidr")
+	assert.Equal(t, "10.0.0.0/16", vpcCIDR)
+
+	// 3. Public Subnets count
+	publicSubnets := terraform.OutputList(t, terraformOptions, "public_subnets")
+	assert.Greater(t, len(publicSubnets), 0)
+
+	// 4. Private Subnets count
+	privateSubnets := terraform.OutputList(t, terraformOptions, "private_subnets")
+	assert.Greater(t, len(privateSubnets), 0)
+
+	// 5. Tags validation
+	tags := terraform.OutputMap(t, terraformOptions, "vpc_tags")
+	assert.Equal(t, "DevOps", tags["Owner"])
+}
